dda6f6abd23b4451c9b817f78df62fc79c9901c2
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-03-02 21:41:30 -0300

    Add files via upload

---
+++ Algoritmos e programação 1/lista de exc2/exc5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+
+    int n1, n2;
+
+        printf("Vamos dizer qual o numero é maior\n ");
+        printf("Insira o primeiro valor\n");
+        scanf("%d", &n1 );
+        printf("Insira o segundo numero \n");
+        scanf("%d", &n2);
+        if (n1>n2) printf("o numero %d eh maior", n1);
+        else
+            printf("o numero %d eh maior", n2);
+
+
+
+
+
+}
