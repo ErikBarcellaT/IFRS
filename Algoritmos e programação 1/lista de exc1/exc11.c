7a077942245d216b0052f6f15df3fa8c570ff486
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-03-21 14:42:46 -0300

    Add files via upload

---
+++ Algoritmos e programação 1/lista de exc1/exc11.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main () {
+
+    float a, b , c , x1, x2, delta ;
+
+ printf("Digite o valor do termo a: ");
+ scanf("%f", &a);
+ printf("Digite o valor do termo b: ");
+ scanf("%f", &b);
+ printf("Digite o valor do termo c: ");
+ scanf("%f", &c);
+ 
+ delta = b*b - 4*a*c;
+ x1 = (-b + sqrt(delta)) / (2*a);
+ x2 = (-b - sqrt(delta)) / (2*a);
+ 
+ if(delta < 0) {
+ printf("A equacao nao possui raizes reais.n");
+ } else {
+ printf("O valor de x1: %.2fn", x1);
+ printf("O valor de x2: %.2fn", x2);
+ }
+ 
+ system("pause");
+ return 0;
+} 
