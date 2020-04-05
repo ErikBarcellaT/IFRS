54a1b99afddb557f9bd51b7e75d166f7a9f0a46a
Erik Barcella <erikbarcellatrisch@gmail.com>
2020-04-05 11:40:53 -0300

    Create exc5.c

---
+++ Algoritmos e programação 1/lista de exc4/exc5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+
+    float notas[5],media;
+
+    while (notas[0,1,2,3,4]!=0) {
+    printf("\n Informe o valor da nota 1: ");
+    scanf("%f", &notas[0]);
+    printf("\n Informe o valor da nota 2: ");
+    scanf("%f", &notas[1]);
+    printf("\n Informe o valor da nota 3: ");
+    scanf("%f", &notas[2]);
+    printf("\n Informe o valor da nota 4: ");
+    scanf("%f", &notas[3]);
+    printf("\n Informe o valor da nota 5: ");
+    scanf("%f", &notas[4]);
+
+    media= ((notas[0]+notas[1]+notas[2]+notas[3]+notas[4])/5); 
+   
+    printf("\n media %.1f ",media ); 
+    } 
+    
+
+    
+    
+
+}
