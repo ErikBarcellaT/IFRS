bfc3d83cb6917255ed6a0bcc1a63e568da572cb0
Erik Barcella <erikbarcellatrisch@gmail.com>
2020-04-12 17:18:17 -0300

    Update exc12.c

--- Algoritmos e programação 1/lista de exc4/exc12.c
+++ Algoritmos e programação 1/lista de exc4/exc12.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-void main () {
+int main(void)
+{
+int n;
+n=0; 
+
+printf("\n Informe um numero inteiro: ");
+scanf ("%d",&n);
 
     
+    if(n%5==0)
+    {
+    printf("\n -numero multiplo de 5");
+    }
+    else
+    {
+    printf("\n  nao e multiplo de 5");
+    }
+
+
+return 0;
 }
