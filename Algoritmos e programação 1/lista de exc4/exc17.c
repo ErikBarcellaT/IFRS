b5a4152c7d0803fd6d35f41352099bd7c9e09d26
Erik Barcella <erikbarcellatrisch@gmail.com>
2020-04-21 22:49:16 -0300

    Create exc17.c

---
+++ Algoritmos e programação 1/lista de exc4/exc17.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define horas_semana 40;
+
+int main () {
+
+    int f, funcionarios[f];
+    float hora_normal,hora_extra,horas_trabalhadas;
+
+    do
+    {
+        printf("\n Informe a quantidade de funcionarios: ");
+        scanf("%d", &f);
+
+        
+
+
+    } while (/* condition */);
+    
+}
