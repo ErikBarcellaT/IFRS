a7ca25fe03ff16890aa80f1445edfeb4acb92657
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-11-10 19:05:07 -0300

    Create exc2.3.js

---
+++ APNPS/Web/Semana 8/exercicios/exc2.3.js
@@ -0,0 +1,24 @@
+var i = 1;
+var j = 1;
+var m;
+var n;
+alert("Divisiveis por 5 são =");
+while (i <=50) {
+m = i % 5;
+if (m == 0) {
+	console.log(i);
+	i =i + 1;
+	    }
+	i = i + 1;
+			
+}
+alert("Divisiveis por 4 são =");
+while (j <48) {
+n = j % 4;
+if (n == 0) {
+	console.log(j);
+	j =j + 1;
+	    }
+	j = j + 1;
+			
+}
