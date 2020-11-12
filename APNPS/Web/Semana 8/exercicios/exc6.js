0ebe4b6645d0ee4b9b26a42344a369907e604ce8
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-11-12 20:07:28 -0300

    Create exc6.js

---
+++ APNPS/Web/Semana 8/exercicios/exc6.js
@@ -0,0 +1,50 @@
+/* var numero_secreto=getRndInteger(1,10);
+var numero_chute;
+
+
+
+while(numero_chute!=numero_secreto){
+  
+    numero_chute=prompt("Adivinhe o numero: ");
+    if(numero_chute==numero_secreto) {
+        alert("Voce acertou, muito bem!!!");
+    } else if(numero_chute>numero_secreto){
+        alert("Muito alto, tente novamente ")
+    } else if(numero_chute<numero_secreto) {
+        alert("Muito baixo, tente novamente");
+    }
+}
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+ */
+
+
+
+var numero_secreto=getRndInteger(1,10);
+var numero_chute;
+var i=0;
+
+
+while(numero_chute!=numero_secreto){
+    while(i<5){
+        i++
+       
+    numero_chute=prompt("Adivinhe o numero: ");
+    if(numero_chute==numero_secreto) {
+        alert("Voce acertou em "+ i + " tentativas, muito bem!!!");
+    } else if(numero_chute>numero_secreto){
+        alert("Muito alto, tente novamente ")
+    } else if(numero_chute<numero_secreto) {
+        alert("Muito baixo, tente novamente");
+    }
+}
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
