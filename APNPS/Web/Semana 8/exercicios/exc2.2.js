f0f3831cf9b4e157a1f3bff6dea30865c3256025
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-11-18 20:17:03 -0300

    Update exc2.2.js

--- APNPS/Web/Semana 8/exercicios/exc2.2.js
+++ APNPS/Web/Semana 8/exercicios/exc2.2.js
@@ -1,6 +1,18 @@
 count=90;
 
 while(count>20) {
+
     console.log(count);
     count=(count-2);
 }
+
+
+/* |||||||
+
+while(count>=20) {
+    if(num/2 == %0)
+    console.log(count);
+    
+}
+
+ */
