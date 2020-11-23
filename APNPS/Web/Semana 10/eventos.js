50e319988cf5e0b78394a24385de8b1d35f1cabb
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-11-23 19:34:03 -0300

    Create eventos.js

---
+++ APNPS/Web/Semana 10/eventos.js
@@ -0,0 +1,5 @@
+var botao = document.querySelector("button");
+var paragrafo= document.querySelector("p")
+botao.addEventListener("click", function() {
+    paragrafo.textContent="alguem clicou no botão"
+})
