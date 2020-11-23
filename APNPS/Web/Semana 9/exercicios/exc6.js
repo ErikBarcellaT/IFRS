5d584a0a53d6ff5d40220e88ef64024b9ab8f4d5
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-11-23 14:35:22 -0300

    Create exc6.js

---
+++ APNPS/Web/Semana 9/exercicios/exc6.js
@@ -0,0 +1,29 @@
+var filme1={
+    nome: "The imitation Game",
+    avaliação: "5 estrelas",
+    status: "visto"
+}
+var filme2={
+    nome: "Hackers",
+    avaliação: "4 estrelas",
+    status: "não visto"
+}
+var filme3={
+    nome: "The Social Network ",
+    avaliação: "3 estrelas",
+    status: "não visto"
+}
+var filme4={
+    nome: "Pirates of Silicon Valley",
+    avaliação: "5 estrelas",
+    status: "visto"
+}
+var filme5={
+    nome: "Revolution OS",
+    avaliação: "4 estrelas",
+    status: "visto"
+}
+
+var filmes =[filme1, filme2, filme3, filme4, filme5];
+
+console.log(filmes)
