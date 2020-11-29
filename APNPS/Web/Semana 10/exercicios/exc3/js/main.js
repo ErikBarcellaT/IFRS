0ec6b49cdbff4ae932587f9d0b1ffc9c63f14369
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-11-29 17:47:06 -0300

    Update main.js

--- APNPS/Web/Semana 10/exercicios/exc3/js/main.js
+++ APNPS/Web/Semana 10/exercicios/exc3/js/main.js
@@ -2,14 +2,22 @@ var botaoMais = document.querySelector("#acrescenta");
 var botaoMenos= document.querySelector("#decrementa");
 var botaoMais2 = document.querySelector("#acrescenta2");
 var botaoMenos2= document.querySelector("#decrementa2");
-var camisetaMasculina = parseInt(document.getElementById("camisetaMasculina").value)
-var camisetaFeminina= parseInt(document.getElementById("camisetaFeminina").value)
+var camisetaMasculina = parseInt(document.getElementById("camisetaMasculina").value);
+var camisetaFeminina= parseInt(document.getElementById("camisetaFeminina").value);
+var total=parseInt(document.getElementById("total").value);
+var concluir=document.querySelector("#concluir");
 
 
 botaoMais.addEventListener("click", aumenta)
 botaoMenos.addEventListener("click", decrementa)
 botaoMais2.addEventListener("click", aumenta2)
 botaoMenos2.addEventListener("click", decrementa2)
+botaoMais.addEventListener("click", totalizando)
+botaoMenos.addEventListener("click", totalizando)
+botaoMais2.addEventListener("click", totalizando)
+botaoMenos2.addEventListener("click", totalizando)
+concluir.addEventListener("click", compra)
+
 
 function aumenta(){
   parseInt(document.getElementById('camisetaMasculina').value=camisetaMasculina+=1);
@@ -31,3 +39,17 @@ function decrementa2(){
   parseInt(document.getElementById('camisetaFeminina').value=camisetaFeminina-=1);
 }
 }
+
+function totalizando(){
+  parseInt(document.getElementById('total').value=(camisetaMasculina*25)+(camisetaFeminina*20))
+
+}
+
+function compra(){
+   if((camisetaFeminina==0)&&(camisetaMasculina==0)){
+    alert("Voce deve selecionar ao menos uma camiseta");
+   } else{
+  alert("Compra realizada com sucesso");
+}
+}
+
