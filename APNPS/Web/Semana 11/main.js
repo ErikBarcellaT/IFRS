6dc60ea3dbb94b3fc1185da01467c46a31bbe69d
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-12-01 20:35:36 -0300

    Create main.js

---
+++ APNPS/Web/Semana 11/main.js
@@ -0,0 +1,15 @@
+var adicionar = document.querySelector('#add')
+
+adicionar.addEventListener("click", adicionarLi)
+
+
+/* function adicionarLi() {
+  var lista  = document.getElementById("pendentes").innerHTML;
+  var newTask=prompt("Informe a nova tarefa: ");
+  lista = lista +"<li>"+ '<input type="checkbox">' + newTask +"</li>";
+  document.getElementById("pendentes").innerHTML = lista;
+} */
+
+function adicionaUm(){
+    console.log("funciona");
+}
