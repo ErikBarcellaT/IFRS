0d80a5d7e5adf5f8ef45203696d2a571bcd0e5f7
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-12-04 19:11:44 -0300

    Update main.js

--- APNPS/Web/Semana 11/main.js
+++ APNPS/Web/Semana 11/main.js
@@ -3,12 +3,26 @@ var adicionar = document.querySelector('#add')
 adicionar.addEventListener("click", adicionarLi)
 
 
-/* function adicionarLi() {
-  var lista  = document.getElementById("pendentes").innerHTML;
-  var newTask=prompt("Informe a nova tarefa: ");
-  lista = lista +"<li>"+ '<input type="checkbox">' + newTask +"</li>";
-  document.getElementById("pendentes").innerHTML = lista;
-} */
+function adicionarLi(){
+    var li = document.createElement("li");
+    var input= document.createElement("input");
+    var edit= document.createElement("button")
+    var delet=document.createElement("button");
+    edit.setAttribute('id', 'delete');
+    delet.setAttribute('id', 'delete');
+    input.setAttribute('type', 'checkbox');
+    var newTask= prompt("Digite a tarefa");
+    var textLi= document.createTextNode(newTask);
+    var textEdit= document.createTextNode("Editar");
+    var textDelet= document.createTextNode("Deletar");
+    edit.appendChild(textEdit);
+    delet.appendChild(textDelet);
+    li.appendChild(input);
+    li.appendChild(textLi);
+    li.appendChild(edit);
+    li.appendChild(delet);
+    document.getElementById("pendentes").appendChild(li);
+}
 
 function adicionaUm(){
     console.log("funciona");
