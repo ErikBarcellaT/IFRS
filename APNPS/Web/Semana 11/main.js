c96b5770a872c825263bf07f9246a1cf6db85966
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-12-06 18:30:50 -0300

    Update main.js

--- APNPS/Web/Semana 11/main.js
+++ APNPS/Web/Semana 11/main.js
@@ -1,29 +1,95 @@
 var adicionar = document.querySelector('#add')
+var tasksPendentes=[];
+var tasksConcluidas=[];
+var newTask;
 
-adicionar.addEventListener("click", adicionarLi)
+
+adicionar.addEventListener('click', addNewTask);
+adicionar.addEventListener('click', addTaskCheck);
+
+
+function addNewTask(){
+    
+    newTask= prompt("Digite a nova tarefa");
+    var li = document.createElement("li");
+    var input= document.createElement("input");
+    input.setAttribute('id', newTask);
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('onclick', valida);
+    var textLi= document.createTextNode(newTask);
+    li.appendChild(input);
+    li.appendChild(textLi);
+    document.getElementById("pendentes").appendChild(li);
+    tasksPendentes.push(li);
+
+   
+}
+    
+
+function valida(){
+
+        if(newTask.checked){
+            console.log('caixa marcada');
+        } else{
+            console.log('caixa não marcada');
+        }
+    }
+
+function addTaskCheck (){
+    concluidas.innerHTML= '<li>' + '<input type=checkbox>' +newTask + '</li>'
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/* var adicionar = document.querySelector('#add')
+var newTask;
+var check= document.getElementById(newTask);
+adicionar.addEventListener("click", adicionarLi);
+check.addEventListener('click', valida);
 
 
 function adicionarLi(){
     var li = document.createElement("li");
     var input= document.createElement("input");
-    var edit= document.createElement("button")
-    var delet=document.createElement("button");
-    edit.setAttribute('id', 'delete');
-    delet.setAttribute('id', 'delete');
     input.setAttribute('type', 'checkbox');
-    var newTask= prompt("Digite a tarefa");
+    newTask= prompt("Digite a tarefa");
+    input.setAttribute('id', newTask);
+    input.setAttribute('classname', 'pend');
     var textLi= document.createTextNode(newTask);
-    var textEdit= document.createTextNode("Editar");
-    var textDelet= document.createTextNode("Deletar");
-    edit.appendChild(textEdit);
-    delet.appendChild(textDelet);
     li.appendChild(input);
     li.appendChild(textLi);
-    li.appendChild(edit);
-    li.appendChild(delet);
     document.getElementById("pendentes").appendChild(li);
+    
 }
 
-function adicionaUm(){
-    console.log("funciona");
+
+
+var arr = []
+
+
+function valida(){
+
+    if(check.checked){
+        console.log('caixa marcada');
+    } else{
+        console.log('caixa não marcada');
+    }
 }
+
+
+
+function adicionarPendentes(){
+    
+}
+
+
+ */
