90e1296e686bbe2a73a014975e1250cad0efd80f
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-12-06 18:32:03 -0300

    Create main.js

---
+++ APNPS/Web/Semana 12/projeto ebdigital/js/main.js
@@ -0,0 +1,23 @@
+var pass = document.querySelector('#pass');
+var newPassword = document.querySelector('#newPassword')
+
+function generatePass () {
+    newPassword.innerHTML='';
+   
+    if(pass.value===''){
+        alert('O tamanho da senha não pode ser igual a zero')
+        return;
+    } else if(pass.value <8) {
+        alert('Não recomendamos uma senha com menos de 8 caracteres');
+        
+    }
+    var charset ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@*?#$";
+    var password ="";
+
+    for(var i =0, n=charset.length; i<pass.value; i++){
+        password += charset.charAt( Math.floor(Math.random()*n));
+    }
+    var result = document.createTextNode(password);
+    return newPassword.appendChild(result);
+
+};
