a3ab625ef35b62608ec230d77879c7e8e9fcecca
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2020-12-14 19:22:28 -0300

    Update main.js

--- APNPS/Web/Semana 11/main.js
+++ APNPS/Web/Semana 11/main.js
@@ -1,54 +1,19 @@
-var adicionar = document.querySelector('#add');
-var elementos = document.querySelectorAll(".pendentes")
-var lerLivro = document.querySelector("#Lerlivro")
+var pendentes = ['estudar', 'programar','aprovar em programação web' ];
+var concluidas = ['feito'];
 
 
-adicionar.addEventListener('click', addNewTask);
-lerLivro.addEventListener('click', valida)
+var ulPendentes = document.getElementById('pendentes');
+var ulConcluidas = document.getElementById('concluidas');
+var adicionar = document.getElementById('add');
 
 
-for(var i of elementos){
-    i.addEventListener('click', function valida(){
-        if(this.checked){
-            console.log('caixa marcada');
-        } else{
-            console.log('caixa não marcada');
-        }
-    }
-    );
-  }
+function atualizaPendentes() {
+    var listaPendentes = '';
+    pendentes.forEach(function (elementos){
+        listaPendentes += '<li> <input type="checkbox" id='cp' + estudar +'</li>'
 
-
-function addNewTask(){
-    newTask= prompt("Digite a nova tarefa");
-    var li = document.createElement("li");
-    var input= document.createElement("input");
-    input.setAttribute('type', 'checkbox');
-    input.setAttribute('class', 'pendentes')
-    var textLi= document.createTextNode(newTask);
-    li.appendChild(input);
-    li.appendChild(textLi);
-    document.getElementById("tasksPend").appendChild(li);
+    });
 }
-
-
-function valida(){
-        if(this.checked){
-            console.log('caixa marcada');
-        } else{
-            console.log('caixa não marcada');
-        }
-    }
-
-
-/* function addTaskCheck (){
-    concluidas.innerHTML= '<li>' + '<input type=checkbox>' + newTask + '</li>'
-
-} */
-
-
-
-
 
 
 
